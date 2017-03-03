621db2c4cde15a861ee83196cffaf3d633864f4b
ktty1220 <ktty1220@gmail.com>
2017-03-03 13:30:12 +0900

    chhc実装中

--- bin/chhc.js
+++ bin/chhc.js
@@ -3,7 +3,9 @@
 
 var readcommand = require('readcommand');
 var ora = require('ora');
+var logSymbols = require('log-symbols');
 var typeOf = require('type-of');
+var valUrl    = require('valid-url');
 var prettyjson = require('prettyjson');
 var Promise = require('rsvp').Promise;
 var colors = require('colors/safe');
@@ -31,27 +33,53 @@ var argv = require('yargs')
 .locale('en')
 .argv;
 
-if (argv.proxy) process.env.HTTP_PROXY = argv.proxy;
-var startUrl = argv._[0];
-if (! /:\/\//.test(startUrl)) {
-  startUrl = 'http://' + startUrl;
-}
+var showData = function (data) {
+  console.info(prettyjson.render(data));
+};
 
-var show = function (obj) {
-  console.info(prettyjson.render(obj));
+var showSuccess = function () {
+  var args = Array.prototype.slice.call(arguments);
+  args.unshift(logSymbols.success);
+  console.info.apply(console, args);
+};
+
+var showError = function (err) {
+  console.error(logSymbols.error, String(err));
 };
 
 var commands = {
   help: {
     description: 'Show help',
-    exec: function (args, $, res, body) {
-      console.info(this);
+    exec: function (args, location) {
+      console.info(Object.keys(commands).map(function (com) {
+        var c = commands[com];
+        var p = colors.yellow((c.params || []).join(' '));
+        return com + ' ' + p + '\n  ' + colors.gray(c.description);
+      }).join('\n'));
+    }
+  },
+  exit: {
+    description: 'Exit chhc',
+    exec: function (args, location) {
+      console.info('bye\n');
+      process.exit();
+    }
+  },
+  setting: {
+    description: 'Show current settings',
+    exec: function (args, location) {
+      var settings = {};
+      Object.keys(client)
+      .filter(function (prop) { return (commands[prop]); })
+      .forEach(function (prop) { settings[prop] = client[prop]; });
+      showData(settings);
     }
   },
   response: {
     description: 'Show response info',
-    exec: function (args, $, res, body) {
-      show({
+    exec: function (args, location) {
+      var res = location.response;
+      showData({
         headers: res.headers,
         cookies: res.cookies
       });
@@ -64,9 +92,13 @@ Object.keys(client).forEach(function (prop) {
   if (type === 'number' || prop === 'maxDataSize') {
     commands[prop] = {
       description: 'Set ' + prop + ' property',
+      params: [ '<number>' ],
       exec: function (args, $, res, body) {
         client.set(prop, parseInt(args[0], 10));
-        console.info('changed ' + prop + ' property to', client[prop]);
+        showSuccess(
+          'changed', colors.magenta(prop),
+          'property to', colors.red(client[prop])
+        );
       }
     };
   }
@@ -75,7 +107,10 @@ Object.keys(client).forEach(function (prop) {
       description: 'Toggle ' + prop + ' property',
       exec: function (args, $, res, body) {
         client.set(prop, ! client[prop]);
-        console.info('changed ' + prop + ' property to', client[prop]);
+        showSuccess(
+          'changed', colors.magenta(prop),
+          'property to', colors.red(client[prop])
+        );
       }
     };
   }
@@ -118,10 +153,31 @@ log
     > links.json
 */
 
+var fetch = function (url, params) {
+  var spinner = ora('fetch ' + url);
+  spinner.start();
+  return client.fetch(url)
+  .then(function (result) {
+    spinner.succeed('ready');
+    return result;
+  })
+  .catch(function (err) {
+    spinner.fail(err.message);
+    throw new Error(err);
+  });
+};
 
-var repl = function ($, res, body) {
+var repl = function (init) {
+  var location = init;
+
   readcommand.loop({
-    ps1: '> '
+    ps1: function () {
+      return [
+        '',
+        colors.cyan(location.$.documentInfo().url),
+        colors.green('》')
+      ].join('\n');
+    }
   }, function (err, args, str, next) {
     if (err && err.code !== 'SIGINT') {
       console.error(colors.red(err.message));
@@ -129,36 +185,38 @@ var repl = function ($, res, body) {
     }
 
     // eslint-disable-next-line eqeqeq, no-eq-null
-    if (str == null) process.exit();
+    if (str == null) {
+      return commands.exit.exec();
+    }
 
     var matches = Object.keys(commands).filter(function (com) {
       return (com.indexOf(args[0]) === 0);
     });
     if (matches.length === 1) {
       args.shift();
-      var ret = commands[matches[0]].exec(args, $, res, body);
+      var ret = commands[matches[0]].exec(args, location);
       if (ret instanceof Promise) {
-        return ret.catch(function (err) {
-          console.error(colors.red(err.message));
-        }).finally(next);
+        return ret.catch(showError).finally(next);
       }
       return next();
     }
 
-    console.info('no such command');
+    if (valUrl.isUri(args[0])) {
+      fetch(args[0]).then(function (result) {
+        location = result;
+      }).finally(next);
+      return null;
+    }
 
+    console.info(logSymbols.warning, 'no such command');
     return next();
   });
 };
 
-var spinner = ora('fetch ' + startUrl);
-spinner.start();
-client.fetch(startUrl)
-.then(function (result) {
-  spinner.succeed('ready');
-  return repl(result.$, result.response, result.body);
-})
-.catch(function (err) {
-  spinner.fail(err.message);
-  process.exit();
-});
+if (argv.proxy) process.env.HTTP_PROXY = argv.proxy;
+var startUrl = argv._[0];
+if (! /:\/\//.test(startUrl)) {
+  startUrl = 'http://' + startUrl;
+}
+
+fetch(startUrl).then(repl).catch(process.exit);
