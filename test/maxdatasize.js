2647881f12a24b6a763c1c106ea6fdc9d22b988d
ktty1220 <ktty1220@gmail.com>
2021-07-22 18:52:11 +0900

    node.js v16での挙動に合わせた調整

--- test/maxdatasize.js
+++ test/maxdatasize.js
@@ -8,24 +8,32 @@ describe('maxdatasize', () => {
   });
 
   test('デフォルトは受信無制限', () => {
-    return new Promise((resolve) => {
+    return new Promise((resolve, reject) => {
       cli.fetch(`${endpoint}/~mega`, (err, $, res, body) => {
-        expect(err).toBeUndefined();
-        expect(body.length).toStrictEqual(1024 * 1024);
-        resolve();
+        try {
+          expect(err).toBeUndefined();
+          expect(body.length).toStrictEqual(1024 * 1024);
+          resolve();
+        } catch (e) {
+          reject(e);
+        }
       });
     });
   });
 
   test('maxDataSizeを指定 => 指定したバイト数で受信制限がかかる', () => {
-    return new Promise((resolve) => {
+    return new Promise((resolve, reject) => {
       cli.set('maxDataSize', 1024 * 64);
       cli.fetch(`${endpoint}/~mega`, (err, $, res, body) => {
-        expect(err.message).toStrictEqual('data size limit over');
-        expect($).toBeUndefined();
-        expect(res).toBeUndefined();
-        expect(body).toBeUndefined();
-        resolve();
+        try {
+          expect(err.message).toStrictEqual('data size limit over');
+          expect($).toBeUndefined();
+          expect(res).toBeUndefined();
+          expect(body).toBeUndefined();
+          resolve();
+        } catch (e) {
+          reject(e);
+        }
       });
     });
   });
